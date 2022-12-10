9595f6d7b3a609d8ac6a527c55e716630ff09d42
Jeferson Siqueira <jefersonlsiq@gmail.com>
2022-12-10 14:19:15 +0000

    adding solutions for day04

---
+++ day04/sol02.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isFullyEnclosed(pair<int, int> a, pair<int, int> b) {
+    if (b.first >= a.first && b.second <= a.second) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool isPartiallyEnclosed(pair<int, int> a, pair<int, int> b) {
+    if (b.first >= a.first && b.first <= a.second) {
+        return true;
+    }
+    return false;
+}
+
+int main() {
+    string input;
+    int count = 0;
+
+    while (cin >> input) {
+        pair<int, int> pa, pb;
+        sscanf(input.c_str(), "%d-%d,%d-%d", &pa.first, &pa.second, &pb.first, &pb.second);
+        if (isFullyEnclosed(pa, pb) || isFullyEnclosed(pb, pa) || isPartiallyEnclosed(pa, pb) || isPartiallyEnclosed(pb, pa)) {
+            count += 1;
+        }
+    }
+
+    cout << count << "\n";
+    return 0;
+}
